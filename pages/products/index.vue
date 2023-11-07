018aeb296e80964149ac3083b98b0fbf8bb2241f
mh.termzee <m.termzee@basecom.com>
2023-11-07 15:09:28 +0100

    add layouts and pages

---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="flex flex-col w-4/5 m-auto justify-center items-start">
+		<h1>Products page</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+			perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
+			Repudiandae.
+		</p>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
