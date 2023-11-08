9fa233d0621ec4e0e87767b46035c8b10b5af87c
mh.termzee <m.termzee@basecom.com>
2023-11-08 16:48:47 +0100

    Generate dynamic SVG stars based on data.rating.rate

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,5 +1,6 @@
 <template>
 	<div
+		v-if="data"
 		class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
 	>
 		<img class="p-8 rounded-t-lg" :src="data?.image" :alt="data?.title" />
@@ -12,61 +13,25 @@
 			</h5>
 
 			<div class="flex items-center mt-2.5 mb-5">
-				<svg
-					class="w-4 h-4 text-yellow-300 mr-1"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="currentColor"
-					viewBox="0 0 22 20"
-				>
-					<path
-						d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-					/>
-				</svg>
-				<svg
-					class="w-4 h-4 text-yellow-300 mr-1"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="currentColor"
-					viewBox="0 0 22 20"
-				>
-					<path
-						d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-					/>
-				</svg>
-				<svg
-					class="w-4 h-4 text-yellow-300 mr-1"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="currentColor"
-					viewBox="0 0 22 20"
-				>
-					<path
-						d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-					/>
-				</svg>
-				<svg
-					class="w-4 h-4 text-yellow-300 mr-1"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="currentColor"
-					viewBox="0 0 22 20"
-				>
-					<path
-						d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-					/>
-				</svg>
-				<svg
-					class="w-4 h-4 text-gray-200 dark:text-gray-600"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="currentColor"
-					viewBox="0 0 22 20"
-				>
-					<path
-						d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-					/>
-				</svg>
+				<!-- Generate dynamic SVG stars based on data.rating.rate -->
+				<div v-for="i in 5" :key="i">
+					<svg
+						class="w-4 h-4"
+						:class="{
+							'text-yellow-300': i <= data?.rating.rate,
+							'text-gray-200 dark:text-gray-600': i > data?.rating.rate,
+						}"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+						viewBox="0 0 22 20"
+					>
+						<path
+							d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+						/>
+					</svg>
+				</div>
+
 				<span
 					class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3"
 				>
--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="text-gray-700 body-font overflow-hidden bg-white">
-		<div class="container px-5 py-24 mx-auto">
+		<div class="container px-5 py-24 mx-auto" v-if="data">
 			<div class="lg:w-4/5 mx-auto flex flex-wrap">
 				<img
 					:alt="data?.title"
@@ -16,71 +16,27 @@
 					</h1>
 					<div class="flex mb-4">
 						<span class="flex items-center">
-							<svg
-								fill="currentColor"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								class="w-4 h-4 text-red-500"
-								viewBox="0 0 24 24"
-							>
-								<path
-									d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
-								></path>
-							</svg>
-							<svg
-								fill="currentColor"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								class="w-4 h-4 text-red-500"
-								viewBox="0 0 24 24"
-							>
-								<path
-									d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
-								></path>
-							</svg>
-							<svg
-								fill="currentColor"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								class="w-4 h-4 text-red-500"
-								viewBox="0 0 24 24"
-							>
-								<path
-									d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
-								></path>
-							</svg>
-							<svg
-								fill="currentColor"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								class="w-4 h-4 text-red-500"
-								viewBox="0 0 24 24"
-							>
-								<path
-									d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
-								></path>
-							</svg>
-							<svg
-								fill="none"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								class="w-4 h-4 text-red-500"
-								viewBox="0 0 24 24"
-							>
-								<path
-									d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
-								></path>
-							</svg>
+							<!-- Generate dynamic SVG stars based on data.rating.rate -->
+							<div v-for="i in 5" :key="i">
+								<svg
+									fill="currentColor"
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									class="w-4 h-4 text-red-500"
+									:class="{
+										'text-red-500': i <= data?.rating.rate,
+										'text-gray-200 dark:text-gray-600': i > data?.rating.rate,
+									}"
+									viewBox="0 0 24 24"
+								>
+									<path
+										d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+									></path>
+								</svg>
+							</div>
+
 							<span class="text-gray-600 ml-3"
 								>{{ data?.rating.count }} Reviews</span
 							>
