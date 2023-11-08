010e1a0a7b2027f462998ccc529d77fdb9332538
mh.termzee <m.termzee@basecom.com>
2023-11-08 16:17:24 +0100

    Custom Error Pages in Nuxt 3 (https://masteringnuxt.com/blog/custom-error-pages-in-nuxt3)

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+	<NuxtLayout>
+		<div class="mt-7 max-w-sm mx-auto text-center">
+			<p>{{ error.statusCode }}</p>
+			<p>{{ error.message }}</p>
+		</div>
+	</NuxtLayout>
+</template>
+
+<script setup lang="ts">
+defineProps(["error"]);
+</script>
+
+<style scoped></style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,11 +9,17 @@ import type { Product } from "~/interfaces/Product";
 
 const { id } = useRoute().params;
 const product = ref<Product>();
+const error = useError();
 
 const fetchProduct = async () => {
 	const res = await fetch(`https://fakestoreapi.com/products/${id}`);
-	const data = await res.json();
-	product.value = data;
+
+	try {
+		const data = await res.json();
+		product.value = data;
+	} catch (err: any) {
+		error.value = err;
+	}
 };
 
 onMounted(() => {
