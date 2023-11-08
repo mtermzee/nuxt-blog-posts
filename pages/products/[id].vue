9a6acaefa998702a5e26ae2ad8b64a02a29c891f
mh.termzee <m.termzee@basecom.com>
2023-11-08 14:24:47 +0100

    fetching Data and add interfaces

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,16 +1,29 @@
 <template>
 	<div class="flex flex-col w-4/5 m-auto justify-center items-start">
-		<p>Product details for {{ id }}</p>
+		<p>Product details for {{ product?.id }}</p>
+		<p>{{ product?.price }} $</p>
+		<p>{{ product?.title }}</p>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-			perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
-			Repudiandae.
+			{{ product?.description }}
 		</p>
 	</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { Product } from "~/interfaces/Product";
+
 const { id } = useRoute().params;
+const product = ref<Product>();
+
+const fetchProduct = async () => {
+	const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+	const data = await res.json();
+	product.value = data;
+};
+
+onMounted(() => {
+	fetchProduct();
+});
 </script>
 
 <style scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,15 +1,28 @@
 <template>
 	<div class="flex flex-col w-4/5 m-auto justify-center items-start">
 		<h1>Products page</h1>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-			perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
-			Repudiandae.
-		</p>
-		<NuxtLink :to="'/products/' + 1">Product Details</NuxtLink>
+		<div class="grid grid-cols-4 gap-5">
+			<div v-for="p in products" :key="p.id">
+				<NuxtLink :to="'/products/' + p.id">{{ p.title }}</NuxtLink>
+			</div>
+		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import type { Product } from "~/interfaces/Product";
+
+const products = ref<Product[]>([]);
+
+const fetchProducts = async () => {
+	const res = await fetch("https://fakestoreapi.com/products");
+	const data = await res.json();
+	products.value = data;
+};
+
+onMounted(() => {
+	fetchProducts();
+});
+</script>
 
 <style scoped></style>
